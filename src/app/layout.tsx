f4238a05d97af962cf666367f08c0ee81667dd13
Diego Gaspar <diegogaspardacruz1@gmail.com>
2023-10-22 12:17:35 -0300

    chore: rename file auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Footer } from '@/components/ui/footer'
 import Header from '@/components/ui/header'
-import { AuthProvider } from '@/providers/auth'
+import { AuthProvider } from '@/providers/auth-provider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
