aa7bbe3d27cee0b82310618a0192099b7ddf952a
Diego Gaspar <diegogaspardacruz1@gmail.com>
2023-10-22 09:33:32 -0300

    feat: add second banner home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -13,23 +13,33 @@ export default async function Home() {
   })
 
   return (
-    <div>
+    <div className="mt-8 flex flex-col gap-8">
       <Image
+        src="/banner-home-01.png"
         className="h-auto w-full px-5"
         sizes="100vw"
-        src="/banner-home-01.png"
         width={0}
         height={0}
         alt="Até 55% de desconto esse mês!"
       />
 
-      <div className="mt-8 px-5">
+      <div className="px-5">
         <Categories />
       </div>
 
-      <div className="mt-8">
+      <div>
+        <h1 className="mb-3 pl-5 font-semibold uppercase">Ofertas</h1>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        width={0}
+        height={0}
+        alt="Até 55% de desconto em mouses!"
+      />
     </div>
   )
 }
