bd7613f40677dc6541d604bb7e2fd362fcd7e63f
Diego Gaspar <diegogaspardacruz1@gmail.com>
2023-10-23 00:50:41 -0300

    feat: add CartItem component and improved cart product addition

---
+++ src/components/ui/cart-item.tsx
@@ -0,0 +1,63 @@
+import { formatCurrencyPtBr } from '@/helpers/format-currency'
+import { CartProduct } from '@/providers/cart-provider'
+import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from 'lucide-react'
+import Image from 'next/image'
+import { Button } from './button'
+
+type CartItemProps = {
+  product: CartProduct
+}
+
+export const CartItem = ({ product }: CartItemProps) => {
+  return (
+    <div className="flex items-center justify-between gap-4">
+      <div className="flex h-[77px] w-[77px] items-center justify-center  rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          width={0}
+          height={0}
+          sizes="100vw"
+          alt={product.name}
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <p className="text-xs">{product.name}</p>
+
+        <div className="flex items-center gap-2">
+          <p className="text-sm font-bold">
+            {formatCurrencyPtBr(product.totalPrice)}
+          </p>
+
+          {product.discountPercentage > 0 && (
+            <p className="text-xs line-through opacity-75">
+              {formatCurrencyPtBr(Number(product.basePrice))}
+            </p>
+          )}
+        </div>
+
+        <div className="flex items-center gap-2 pt-[5px]">
+          <Button
+            className="h-8 w-8"
+            size="icon"
+            variant="outline"
+            disabled={product.quantity === 1}
+          >
+            <ArrowLeftIcon size={16} />
+          </Button>
+
+          <span className="text-sm">{product.quantity}</span>
+
+          <Button className="h-8 w-8" size="icon" variant="outline">
+            <ArrowRightIcon size={16} />
+          </Button>
+        </div>
+      </div>
+
+      <Button size="icon" variant="outline">
+        <TrashIcon size={16} />
+      </Button>
+    </div>
+  )
+}
