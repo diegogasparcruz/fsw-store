d7f5b746d79218b2c941a14018cdeb5f7fdbe7c0
Diego Gaspar <diegogaspardacruz1@gmail.com>
2023-10-23 01:50:54 -0300

    feat: ensure cart product can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -3,14 +3,16 @@ import { computeProductTotalPrice } from '@/helpers/products'
 import { useCartContext } from '@/providers/cart-provider'
 import { ShoppingCartIcon } from 'lucide-react'
 import { Badge } from './badge'
+import { Button } from './button'
 import { CartItem } from './cart-item'
+import { ScrollArea } from './scroll-area'
 import { Separator } from './separator'
 
 export const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useCartContext()
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -19,19 +21,23 @@ export const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem
-              key={product.id}
-              product={computeProductTotalPrice(product) as any}
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">
-            Carrinho vazio. Vamos fazer compras?
-          </p>
-        )}
+      <div className="h-full max-h-full overflow-hidden">
+        <ScrollArea className="h-full ">
+          <div className="flex h-full flex-col gap-5">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">
+                Carrinho vazio. Vamos fazer compras?
+              </p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
 
       {products.length > 0 && (
@@ -63,6 +69,8 @@ export const Cart = () => {
             <p>Total</p>
             <p>{formatCurrencyPtBr(total)}</p>
           </div>
+
+          <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
         </div>
       )}
     </div>
