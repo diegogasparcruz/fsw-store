676bdb65c3d0601c98839c4574cfdedbbe4b844c
Diego Gaspar <diegogaspardacruz1@gmail.com>
2023-10-22 23:33:38 -0300

    feat: add recommended products title to product details page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,8 @@
 import { ProductList } from '@/components/ui/product-list'
+import { SectionTitle } from '@/components/ui/section-title'
 import { prismaClient } from '@/lib/prisma'
 import { Categories } from './components/categories'
 import { PromoBanner } from './components/promo-banner'
-import { SectionTitle } from './components/section-title'
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
