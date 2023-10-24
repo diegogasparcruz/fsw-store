66c1ba83caa14ed0016a4130d1b19d0918986ebe
Diego Gaspar <diegogaspardacruz1@gmail.com>
2023-10-24 00:15:38 -0300

    style: change width of product item and spancing on cart item

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -29,52 +29,53 @@ export const CartItem = ({ product }: CartItemProps) => {
 
   return (
     <div className="flex items-center justify-between gap-4">
-      <div className="flex h-[77px] w-[77px] items-center justify-center  rounded-lg bg-accent">
-        <Image
-          src={product.imageUrls[0]}
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt={product.name}
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-        />
-      </div>
-
-      <div className="flex flex-col">
-        <p className="text-xs">{product.name}</p>
-
-        <div className="flex items-center gap-2">
-          <p className="text-sm font-bold">
-            {formatCurrencyPtBr(product.totalPrice)}
-          </p>
-
-          {product.discountPercentage > 0 && (
-            <p className="text-xs line-through opacity-75">
-              {formatCurrencyPtBr(Number(product.basePrice))}
-            </p>
-          )}
+      <div className="flex items-center gap-4">
+        <div className="flex h-[77px] w-[77px] items-center justify-center  rounded-lg bg-accent">
+          <Image
+            src={product.imageUrls[0]}
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt={product.name}
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          />
         </div>
+        <div className="flex flex-col">
+          <p className="text-xs">{product.name}</p>
 
-        <div className="flex items-center gap-2 pt-[5px]">
-          <Button
-            className="h-8 w-8"
-            size="icon"
-            variant="outline"
-            onClick={handleDecreaseProductQuantityClick}
-          >
-            <ArrowLeftIcon size={16} />
-          </Button>
+          <div className="flex items-center gap-2">
+            <p className="text-sm font-bold">
+              {formatCurrencyPtBr(product.totalPrice)}
+            </p>
 
-          <span className="text-sm">{product.quantity}</span>
+            {product.discountPercentage > 0 && (
+              <p className="text-xs line-through opacity-75">
+                {formatCurrencyPtBr(Number(product.basePrice))}
+              </p>
+            )}
+          </div>
 
-          <Button
-            className="h-8 w-8"
-            size="icon"
-            variant="outline"
-            onClick={handleIncreaseProductQuantityClick}
-          >
-            <ArrowRightIcon size={16} />
-          </Button>
+          <div className="flex items-center gap-2 pt-[5px]">
+            <Button
+              className="h-8 w-8"
+              size="icon"
+              variant="outline"
+              onClick={handleDecreaseProductQuantityClick}
+            >
+              <ArrowLeftIcon size={16} />
+            </Button>
+
+            <span className="text-sm">{product.quantity}</span>
+
+            <Button
+              className="h-8 w-8"
+              size="icon"
+              variant="outline"
+              onClick={handleIncreaseProductQuantityClick}
+            >
+              <ArrowRightIcon size={16} />
+            </Button>
+          </div>
         </div>
       </div>
 
